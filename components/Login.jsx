f6f1189337a91c8ff8b8e4550515bdb858ceb3ef
truchlewski <132082398+truchlewski@users.noreply.github.com>
2023-12-07 00:28:31 +0100

    logowanie

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,35 +1,57 @@
 import React, { useState } from 'react';
 import styles from "../styles/Login.module.css"
 
-export default  function Login() {
-    // Tutaj można dodać logikę stanu, jeśli jest potrzebna
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:5000/api/auth', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: username, password: password }),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                // Redirect to posts form page
+                window.location.href = 'http://localhost:3000/posts/form';
+            } else {
+                alert(data.message); // Show error message
+            }
+        } catch (error) {
+            console.error('Login error:', error);
+        }
+    }
 
     return (
         <div className='logos'>
-        <div className={styles.wrapper}>
-            <div className={styles.logo}>
-                <img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png" alt="Twitter Logo" />
+            <div className={styles.wrapper}>
+                
+                <form className="p-3 mt-3" onSubmit={handleSubmit}>
+                    <div className="form-field d-flex align-items-center">
+                        <span className="far fa-user"></span>
+                        <input type="text" name="userName" id="userName" placeholder="Username" 
+                               value={username} onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div className="form-field d-flex align-items-center">
+                        <span className="fas fa-key"></span>
+                        <input type="password" name="password" id="pwd" placeholder="Password" 
+                               value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <button className="btn mt-3" type="submit">Login</button>
+                </form>
+                
             </div>
-            <div className="text-center mt-4 name">
-                Twitter
-            </div>
-            <form className="p-3 mt-3">
-                <div className="form-field d-flex align-items-center">
-                    <span className="far fa-user"></span>
-                    <input type="text" name="userName" id="userName" placeholder="Username" />
-                </div>
-                <div className="form-field d-flex align-items-center">
-                    <span className="fas fa-key"></span>
-                    <input type="password" name="password" id="pwd" placeholder="Password" />
-                </div>
-                <button className="btn mt-3">Login</button>
-            </form>
-            <div className="text-center fs-6">
-                <a href="#">Forget password?</a> or <a href="#">Sign up</a>
-            </div>
-        </div>
         </div>
     );
 }
+
 
 
