5fa52e3c496c674dc09c6b30ed3b89227da23309
truchlewski <132082398+truchlewski@users.noreply.github.com>
2023-12-16 19:09:45 +0100

    porzadki

--- server/index.js
+++ server/index.js
@@ -1,30 +1,44 @@
 import express from 'express';
-import cors from "cors";
+import cors from 'cors';
+import mysql from 'mysql';
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from 'url';
+import pagesRouter from './routes/pages.js';
+
+
+dotenv.config({path: './.env'});
 
 const app = express();
 const PORT = 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(cors());
-app.use(express.json()); 
-
-app.get("/api/home", (req, res) => {
-    res.json({ });
+// Konfiguracja połączenia z bazą danych
+const database = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD, 
+    database: process.env.DATABASE
 });
-
-// Dodajemy endpoint do logowania
-app.post("/api/auth", (req, res) => {
-    const { email, password } = req.body;
-    
-    // Tutaj dodaj logikę weryfikacji danych logowania, na przykład sprawdzając w bazie danych
-    // Dla uproszczenia używamy stałych wartości
-    if (email === "test@ex.com" && password === "password") {
-        // Logowanie pomyślne
-        res.json({ message: "Login successful!" });
+const publicDirectory =  path.join(__dirname,)
+console.log(__dirname);
+// Nawiązywanie połączenia z bazą danych
+database.connect((error) => {
+    if (error) {
+        console.log(error);
     } else {
-        // Logowanie nieudane
-        res.status(401).json({ message: "Invalid credentials!" });
+        console.log("MYSQL Connected...");
     }
 });
+
+app.use(cors());
+app.use(express.json());
+
+// w tym momwncie sciagamy z pages componenty
+app.use('/', pagesRouter);// w tym miejscu wymagamy z pages strony
+
+
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
